0ada5648abd0312bff685b6f2419bcce974f304e
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2025-10-24 17:39:56 +0700

    .

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <script>document.write(new Date().getFullYear())</script> &copy; {{ config('app.name') }}.
+                <script>document.write(new Date().getFullYear())</script> &copy; Videa School Payment System. All rights reserved by IDN PT.
             </div>
         </div>
     </div>
