61f0c4cf510872e998f3088459fcf87f2670a4e9
panjidev <panjidenisgiantoroo@gmail.com>
2025-11-07 21:33:38 +0700

    .

---
+++ database/migrations/2025_11_07_085221_fix_akuns_sequence.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        \Illuminate\Support\Facades\DB::statement("SELECT setval('akuns_id_seq', (SELECT MAX(id) FROM akuns))");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
