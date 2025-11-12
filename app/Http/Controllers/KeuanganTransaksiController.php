57bfab5ddf806ed2f3ba11906a8c3c7ae95f7f0d
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2025-11-12 18:04:37 +0700

    .

--- app/Http/Controllers/KeuanganTransaksiController.php
+++ app/Http/Controllers/KeuanganTransaksiController.php
@@ -437,7 +437,8 @@ class KeuanganTransaksiController extends Controller
                     $tagihanSiswa->update([
                         'status' => $statusBaru,
                         'sisa_nominal' => $sisaNominalBaru,
-                        'jumlah_dibayar' => $jumlahDibayarBaru,
+//                        'jumlah_dibayar' => $jumlahDibayarBaru,
+                        'tanggal_bayar' => now(),
                         'status_approval' => 'approved',
                         'status_verifikasi' => 'approved'
                     ]);
