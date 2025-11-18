e6b56c3fa275c0733ce38384dd645b8f5dd650c6
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2025-11-18 16:53:53 +0700

    .

--- app/Http/Controllers/Api/V1/RiwayatApiController.php
+++ app/Http/Controllers/Api/V1/RiwayatApiController.php
@@ -159,6 +159,8 @@ class RiwayatApiController extends Controller
                     'verified_by' => $trx->verifier ? $trx->verifier->name : null,
 //                    'verified_at' => $trx->verified_at ? $trx->verified_at->format('Y-m-d H:i:s') : null,
                     'catatan_verifikasi' => $trx->catatan_verifikasi,
+                    'token' => $trx->token,
+                    'expired_kode' => $trx->token_expired_at ? $trx->token_expired_at->format('Y-m-d H:i:s') : null,
                 ];
             });
 
