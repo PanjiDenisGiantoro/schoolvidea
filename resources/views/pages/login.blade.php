1e447239532b97ca8974580739f45f44ebe72379
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2025-11-24 16:25:06 +0700

    .docker 5

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -69,7 +69,7 @@
 
                                 {{-- Footer text --}}
                                 <p class="text-muted mb-0 mt-4 text-center">
-                                    &copy; {{ date('Y') }} VideaClass by PT. Inovasi Dalam Negeri - All Rights Reserved
+                                    &copy; {{ date('Y') }} VideaClass by PT. Inovasi Dalam Negeri - All Rights Reserveds
 
                                 </p>
                             </div>
