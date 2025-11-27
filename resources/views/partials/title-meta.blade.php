3eab643534000b45b6fb2e607587a6c3d16d53c1
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2025-11-27 17:50:14 +0700

    .docker 7

--- resources/views/partials/title-meta.blade.php
+++ resources/views/partials/title-meta.blade.php
@@ -5,4 +5,4 @@
 <meta name="description" content="{{ config('app.name') }}" />
 <meta name="author" content="FoxPixel" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<link rel="shortcut icon" href="assets/images/favicon.ico">
+<link rel="shortcut icon" href="{{ asset('assets/images/videa.png') }}">
