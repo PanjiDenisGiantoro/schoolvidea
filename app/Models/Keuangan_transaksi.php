281922212ddeaf55a334d003402b31f7bd01a424
kamilhiber22 <raafikamil24@gmail.com>
2026-01-09 13:37:55 +0700

    fix(1): fix keuangan transaksi

--- app/Models/Keuangan_transaksi.php
+++ app/Models/Keuangan_transaksi.php
@@ -59,7 +59,7 @@ class Keuangan_transaksi extends Model
     public function scopeHidePendingWithoutBukti($query)
     {
         return $query->where(function ($q) {
-            $q->where('status_verifikasi', '=', 'pending')
+            $q->where('status_verifikasi', '!=', 'pending')
 
               ->orWhere(function ($sub) {
                   $sub->where('status_verifikasi', 'pending')
@@ -85,7 +85,7 @@ class Keuangan_transaksi extends Model
     }
     public function scopeHidePending($query) {
         return $query->where(function ($q) {
-            $q->where('status_verifikasi', '=', 'pending');
+            $q->where('status_verifikasi', '!=', 'pending');
         });
     }
 }
